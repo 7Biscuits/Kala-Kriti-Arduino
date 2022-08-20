d8679696a4205c47d8dab8f41fd818e5fa186295
BiscuitButB <RudranshSrivastava2525@gmail.com>
2022-08-20 15:46:55 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,32 +1,32 @@
-#include <Adafruit_LiquidCrystal.h> //  This is Library for Oled 
-#define OLED_RESET 4 //  This for reset. It is not Important to write
+#include <Adafruit_LiquidCrystal.h> 
+
 Adafruit_LiquidCrystal lcd(0);
 int sensor = A0;
 int buzzer = 2;
 int red = 10;
 int green = 8;
 int blue = 9;
-int gasLevel = 0; //int variable for gas level
+int gasLevel = 0; 
 String quality = "";
 
 void setup() {
-
-  Serial.begin(9600); //start serial comms
-  pinMode(buzzer, OUTPUT); //set beeper for output
-  pinMode(sensor, INPUT); //set sensor for input
-  pinMode (red, OUTPUT);
-  pinMode (green, OUTPUT);
-  pinMode (blue, OUTPUT);
-  lcd.begin(16, 2); // This is the address of our display so dont change it.
-  lcd.clear(); // This is for clear Oled. If you will not this is print Adafruit Logo
-  lcd.setBacklight(1); // This is color of text without this you can't see text
-  lcd.println("KALA-KRITI");
-  lcd.display(); // THIS IS ALWAYS USE AT LAST OF CODING AFTER PRINT ANYTHING IN OLED
+  
+  Serial.begin(9600); 
+  pinMode(buzzer, OUTPUT); 
+  pinMode(sensor, INPUT); 
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  lcd.begin(16, 2); 
+  lcd.clear(); 
+  lcd.setBacklight(1); 
+  lcd.setCursor(0, 1);
+  lcd.print("KALA-KRITI");
   delay(2000);
 }
 
 void loop() {
-
+  
   gasLevel = analogRead(sensor);
   Serial.print(" Gas Level:");
   Serial.println(gasLevel);
@@ -35,34 +35,31 @@ void loop() {
   digitalWrite (blue, LOW);
   digitalWrite (buzzer, LOW);
 
-  if (gasLevel < 150) {
+  if (gasLevel <= 320) {
     quality = "GOOD";
     digitalWrite (green, HIGH);
-    delay(500);
+    delay(100);
   }
 
-  else if (gasLevel > 150 and gasLevel < 250) {
+  else if (gasLevel > 320 and gasLevel < 700) {
     quality = "Unhealthy";
     digitalWrite (blue, HIGH);
-    tone(buzzer, 800, 50);
-    delay(200);
+    tone(buzzer, 500, 50);
+    delay(100);
   }
 
-  else if (gasLevel > 250) {
+  else if (gasLevel > 700) {
     quality = "Hazardous ";
     digitalWrite (red, HIGH);
-    digitalWrite( buzzer, HIGH);
-    delay(200);
+    tone( buzzer, 800, 1000);
+    delay(100);
   }
 
   lcd.clear();
   lcd.setBacklight(1);
   lcd.setCursor(0, 0);
-  lcd.print("Air Quality is: ");
-  lcd.setCursor(100, 0);
-  lcd.print(gasLevel);
-  lcd.setCursor(0, 15);
-  lcd.println(quality);
-  lcd.display();
+  lcd.print("Air Quality is");
+  lcd.setCursor(0, 1);
+  lcd.print(quality);
   delay(500);
 } 
